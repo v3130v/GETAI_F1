673db2a512d14cc6bd890428aac471543c9ecf7d
v3130v <v3130v@gmail.com>
2025-10-08 19:16:38 +0300

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -6,7 +6,7 @@
 import '@tailwindcss/browser';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {App} from './App';
+import {App} from './App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
